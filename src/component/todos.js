27b88eac86fa1dfc904ca03883787e9ce7c6d426
Gbenuola <golaiya@andrew.cmu.edu>
2022-06-20 16:54:46 -0400

    Final week 2 submission. All Tests pass

--- src/component/todos.js
+++ src/component/todos.js
@@ -16,7 +16,7 @@ const Todos = ({ todos, deleteTodo }) => {
       return (
         <Grid key={todo.id}>
           <Card>
-            { todo.content/* Remember, we set the local state of this todo item when the user submits the form in 
+            {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} */}
               <ListItemButton component="a" href="#simple-list">
                 <Checkbox style={{ paddingLeft:0 }} color="primary" onClick={() => {deleteTodo(todo.id)}}/>
